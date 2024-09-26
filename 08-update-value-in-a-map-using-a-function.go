63ab4b79d130957e5a2fe940a42d0ea33f739fc1
garagaradev <garagaradev@proton.me>
2024-09-26 15:03:54 +0700

    update value in a map using a function

---
+++ 08-update-value-in-a-map-using-a-function.go
@@ -0,0 +1,20 @@
+// write a function that takes a map of string to int and adds 10 to each value
+package main
+import "fmt"
+
+func addTenToEachValue(m map[string]int) {
+  for key := range m {
+    m[key] += 10
+  }
+}
+
+func main(){
+  fruits := map[string] int {
+    "apple":5,
+    "banana":2,
+    "orange":8,
+  }
+  fmt.Println("Initial map:", fruits)
+  addTenToEachValue(fruits)
+  fmt.Println("Current map:", fruits)
+}
